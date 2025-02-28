6d1e7023a0e839c790950fe334e3e0e99d02ac14
p2z29 <zayroxzanted257@gmail.com>
2025-02-28 01:57:04 -0600

    p2z29.kt

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/kotlin/p2z29.kt
@@ -0,0 +1,21 @@
+//https://kotlinlang.org/
+//Esta es la froma de escribir un comentario de una linea o renglon (//)
+/*Esta es la forma de escribir
+ * un comentario
+ * de un renglon
+ * o más (/**/)
+ * */
+fun main() {
+    val constante = 21
+    //NUMERICOS
+    val Intv: Int = 17
+    val Doublev: Double = 29.7
+    val Floatv: Float = 2.61F
+    //CADENA
+    val Stringv: String = "kotlin"
+    //BOOLEANO
+    val Booleanv: Boolean = true
+    //CARACTERES
+    val Charv: Char = 'I'
+    println("¡Hola kotlin!")
+}
